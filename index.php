fa45a9d3e228b22548b9077626879e753be3b86b
Rafael Trancoso <rbfox21@yahoo.com.br>
2019-12-09 10:05:34 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+	$categorias = [];
+	$categorias[] = 'Infantil';
+	$categorias[] = 'Adolescente';
+	$categorias[] = 'Adulto';
+	//print_r($categorias);
+	$nome = 'FULANO';
+	$idade = 13;
+	
+	//var_dump($nome);
+	//var_dump($idade);
+	
+	if ($idade >= 6 && $idade <= 12){
+		echo 'O nadador ' . $nome . ' compete na categoria ' . $categorias[0];
+	}else if ($idade >= 13 && $idade < 18){
+		echo 'O nadador ' . $nome . ' compete na categoria ' . $categorias[1];
+	}else{
+		echo 'O nadador ' . $nome . ' compete na categoria ' . $categorias[2];
+	}
+?>
